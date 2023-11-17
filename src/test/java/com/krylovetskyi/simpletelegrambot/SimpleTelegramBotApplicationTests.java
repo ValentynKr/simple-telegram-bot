0b5854bb0ceabaf741fa78e7bbfaee85c2974273
Valentyn Krylovetskyi <valentyn_krylovetskyi@epam.com>
2023-11-17 13:03:53 +0200

    Initial commit

---
+++ src/test/java/com/krylovetskyi/simpletelegrambot/SimpleTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krylovetskyi.simpletelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
